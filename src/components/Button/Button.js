f2ec43634eff97dd543d7f82599a83b4824719c7
Artem Zayarny <Timaz@Artems-MacBook-Pro.local>
2020-12-08 22:28:59 +0200

    Fixed first sequence

--- src/components/Button/Button.js
+++ src/components/Button/Button.js
@@ -1,67 +1,23 @@
 import React, { useEffect, useState } from 'react'
 import PropTypes from 'prop-types'
 import styles from './Button.module.scss'
-import green from '../../sounds/1.mp3'
-import red from '../../sounds/2.mp3'
-import blue from '../../sounds/3.mp3'
-import yellow from '../../sounds/4.mp3'
+
 
 
 Button.propTypes = {
-    color: PropTypes.string
+    color: PropTypes.string,
+    isPlay: PropTypes.bool,
 }
 
 
 Button.defaultProps = {
-    color: 'gray'
+    color: 'gray',
+    isPlay: false
 }
-export default function Button({ color }) {
-    const [play, setPlay] = useState(false)
-    const sequence = ['blue'];
+export default function Button({ color, handlePlay, isPlay }) {
 
 
-    // useEffect(() => {
-    //     playSequence(sequence)
-    // }, [])
-
-    // const playSequence = (sequence) => {
-    //     sequence.forEach((button) => {
-    //         playSound(setAudio(button))
-    //     })
-    // }
-
-
-    const setAudio = (color) => {
-        let audio = null;
-        switch (color) {
-            case 'green':
-                audio = new Audio(green);
-                break;
-            case 'blue':
-                audio = new Audio(blue);
-                break;
-            case 'red':
-                audio = new Audio(red);
-                break;
-            case 'yellow':
-                audio = new Audio(yellow);
-                break;
-        }
-        return audio
-    }
-    const playSound = audioFile => {
-        console.log()
-        audioFile.play();
-        audioFile.onended = () => { console.log('end') }
-    }
-    const handlePlay = () => {
-        setPlay(true)
-        playSound(setAudio(color))
-        setInterval(() => {
-            setPlay(false)
-        }, 500)
-    }
     return (
-        <button onClick={handlePlay} className={`${styles.btn} ${styles[color]} ${play ? styles['play'] : ''}`}></button>
+        <button onClick={handlePlay} className={`${styles.btn} ${styles[color]} ${isPlay ? styles['play'] : ''}`}></button>
     )
 }
--- src/components/Pad/Pad.js
+++ src/components/Pad/Pad.js
@@ -1,14 +1,49 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import Button from '../Button/Button'
 import styles from './Pad.module.scss'
+import green from '../../sounds/1.mp3'
+import red from '../../sounds/2.mp3'
+import blue from '../../sounds/3.mp3'
+import yellow from '../../sounds/4.mp3'
 
 export default function Pad(props) {
+
+    const [play, setPlay] = useState(false)
+    const [color, setColor] = useState('')
+
+    const sequence = ['blue', 'red', 'green']
+
+    useEffect(() => {
+        // hilight('blue')
+        playSequence(sequence)
+    }, [])
+
+    const hilight = async (color) => {
+        return new Promise((resolve, reject) => {
+            console.log('color', color)
+            setPlay(true)
+            setColor(color)
+            setTimeout(() => {
+                setPlay(false)
+                setColor('')
+                resolve();
+            }, 1000);
+        })
+
+    }
+
+    const playSequence = async (sequence) => {
+        for (const color of sequence) {
+            await hilight(color)
+        }
+    }
+
     return (
         <div className={`${styles.pad}`}>
-            <Button color="red" />
-            <Button color="green" />
-            <Button color="blue" />
-            <Button color="yellow" />
+            <Button isPlay={play && color === 'green'} color="green" />
+            <Button isPlay={play && color === 'red'} color="red" />
+            <Button isPlay={play && color === 'blue'} color="blue" />
+            <Button isPlay={play && color === 'yellow'} color="yellow" />
         </div>
     )
 }
